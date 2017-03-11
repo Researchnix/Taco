3e0a0ea12adac884248a7b11346de54e7ec54783
Researchnix <forschnix@gmail.com>
2017-03-11 15:08:25 +0100

    "update"

---
+++ main.py
@@ -0,0 +1,38 @@
+#
+#  main.py
+#  HackThe6ix
+#
+#  Created by Jeffrey, Maliha, Justin  and Lennart on 2016-08-06.
+#  Copyright 2016 Researchnix. All rights reserved.
+#
+
+import sys
+import time
+
+import Painter
+import RandomGenerator
+#import visualization
+from math import *
+
+
+
+if __name__ == "__main__":
+    t = time.time()
+
+    # Initialize the Master and check its state
+    #mas = Master.Master()
+    #mas.printState()
+    #mas.run()
+    #visualization.visualization()
+
+    '''
+    rage = RandomGenerator.RandomGenerator()
+    rage.generateInter(30)
+    rage.writeToFile()
+    '''
+    pain = Painter.Painter()
+    pain.mainloop()
+
+
+
+    print "\n\nDone in " + str(time.time() - t) + " s"
