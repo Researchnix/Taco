3745aaf87d881d451af219694efa2f742f087307
Mattriplex <marosini@bluewin.ch>
2017-03-12 11:58:06 +0100

    Implemented all specified functionality

--- Street.py
+++ Street.py
@@ -13,7 +13,6 @@ class Street:
     ID = ""
     length = 10
     carAmount = 0
-    percentFilled = 0.0
     flexQueue = collections.deque([])
     standingQueue = collections.deque([])
 
@@ -23,25 +22,37 @@ class Street:
         self.ID = ID
         self.length = length
 
-
-
+#Adds a Car at the end of the queue, returns true if successful, false if street is full
     def queueCar(self, c):
         if self.carAmount >= self.length:
             return False
         else:
+            flexFirst = self.flexQueue.popleft()
+            if flexFirst is not None:
+                self.standingQueue.append(flexFirst)
             self.flexQueue.append(c)
-
             self.carAmount += 1
             return True
 
 
     def dequeueCar(self):
+        self.carAmount -= 1
+        self.flexQueue.append(None)
+        return self.standigQueue.popleft()
 
     def update(self):
+        flexFirst = self.flexQueue.popleft()
+        if flexFirst is None:
+            self.flexQueue.append(None)
+        else:
+            self.standingQueue.append(flexFirst)
 
 
 
     def getCarAmount(self):
+        return self.carAmount
 
+    def getPctFull(self):
+        return (float)(self.carAmount) / self.length
 
 
