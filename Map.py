8553e82b8c02072bf81ff9e5e7adc0d16e9cac6d
Mattriplex <marosini@bluewin.ch>
2017-03-12 14:20:51 +0100

    added bugs

--- Map.py
+++ Map.py
@@ -5,3 +5,20 @@
 #  Created by Mat, Kon and Len on 2017-03-11.
 #  Copyright 2016 Researchnix. All rights reserved.
 #
+import Intersection
+import Street
+
+class Map:
+
+    intersections = []
+    streets = []
+
+    def __init__(self, fileStreets, fileInter):
+
+        f = open(fileInter, 'r')
+        for line in f:
+            line = line.split()
+            x = Intersection()
+            self.intersections.append(Intersection(int(line[0]), int(line[1]), int(line[2])))
+
+
