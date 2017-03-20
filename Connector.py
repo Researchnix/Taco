88f2483e5b36467322891a9d8abd420d4d768107
Researchnix <forschnix@gmail.com>
2017-03-20 19:23:51 +0100

    "update"

---
+++ Connector.py
@@ -0,0 +1,19 @@
+#
+#  Connector.py
+#  Taco  ---  SPH Innovation Challenge
+#
+#  Created by Mat, Kon and Len on 2017-03-11.
+#  Copyright 2016 Researchnix. All rights reserved.
+#
+
+import Vector
+
+class Connector:
+
+
+
+
+    def connect(self, streets, intersections):
+        vectors = {}
+
+
---
+++ Vector.py
@@ -0,0 +1,21 @@
+#
+#  Vector.py
+#  Taco  ---  SPH Innovation Challenge
+#
+#  Created by Mat, Kon and Len on 2017-03-11.
+#  Copyright 2016 Researchnix. All rights reserved.
+#
+
+class Vector:
+
+    fromPos = None
+    toPos = None
+
+    def __init__(self, fromX, fromY, toX, toY):
+        self.fromPos = [fromX, fromY]
+        self.toPos = [toX, toy]
+
+
+
+    def angle(self, vector):
+        return 0
